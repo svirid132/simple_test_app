002c511e7ecbc7bbe2fe62163d1f873deaa40e63
svirid132 <svirid132@gmail.com>
2021-05-11 01:02:45 +0300

    repeat server

--- git cmd/update_client.bat
+++ git cmd/update_client.bat
@@ -1,11 +1,11 @@
 @echo off
 
 rem settings directory
-set mydrive=D:\
-set mypath=Project\Portfolio\Test
+set mydrive=D:
+set mypath=\Project\Portfolio\Test
 set pathToServer=E:/Project_gitServer/Test
-cd mydrive
-cd mypath
+cd %mydrive%
+cd %mypath%
 
 git pull %pathToServer%
 pause
